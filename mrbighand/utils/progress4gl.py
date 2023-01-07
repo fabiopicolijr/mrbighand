42322747a8c7ff838e45b7a59ee66770d466d6c2
Fábio Picoli <fabiopicolijr@gmail.com>
2023-01-07 11:19:00 -0300

    create_temp_table_definition

--- mrbighand/utils/progress4gl.py
+++ mrbighand/utils/progress4gl.py
@@ -1,4 +1,16 @@
-class Progress4GL:
 
+class Progress4GL:
+    """
+    Progress4GL
+    Responsible for creation of progress 4GL commands
+    """
     def __init__(self):
+        pass
+
+    def create_temp_table_definition(self, name, fields: tuple, indexes: dict):
+        """
+        Progress4GL
+        Responsible for creation of temp-table definition
+        """
+
         pass
