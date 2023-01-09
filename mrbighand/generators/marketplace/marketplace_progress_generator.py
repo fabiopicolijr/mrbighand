342a962db8afe72c79b6c06bb15b166bf0217650
Fábio Picoli <fabiopicolijr@gmail.com>
2023-01-09 12:00:56 -0300

    filename fix

--- mrbighand/generators/marketplace/marketplace_progress_generator.py
+++ mrbighand/generators/marketplace/marketplace_progress_generator.py
@@ -14,9 +14,7 @@ class MarketplaceProgressGenerator(Progress4GL):
 
         self.tree = schema_tree
         self.progress4gl = Progress4GL()
-        self.file_dot_i = f"./{OUTPUT_PATH}/{GENERATOR_MODULE}/{context.setup.api_name}.i"
-        self.output = f'./{OUTPUT_PATH}/{GENERATOR_MODULE}/{context.setup.api_name}'
-        self.files = {'dot_i': f"{self.output}/{context.setup.api_name}.i"}
+        self.output_folder = f'./{OUTPUT_PATH}/{GENERATOR_MODULE}/{context.setup.api_name}'
 
     def generate(self):
         # TODO: 1. Generate .i file (temp table definition)
@@ -28,6 +26,7 @@ class MarketplaceProgressGenerator(Progress4GL):
         # TODO: 4. Generate .p file
 
     def generate_dot_i(self):
+        dot_i_file = f"{self.output_folder}/{context.setup.api_name}.i"
         temp_tables = []
         array_nodes = self.tree.get_nodes_by_type(list)
 
@@ -36,20 +35,18 @@ class MarketplaceProgressGenerator(Progress4GL):
             temp_table = self.progress4gl.create_temp_table_definition(self.tree[node_id].identifier, related_leaves)
             temp_tables.append(temp_table)
 
-        write_file(self.files['dot_i'], temp_tables, separator="\n\n")
+        write_file(dot_i_file, temp_tables, separator="\n\n")
 
     def generate_json_funcs(self):
-        data = []
+        # data = []
+        #
+        # for node_identifier in self.tree.expand_tree(mode=TreeManager.WIDTH):
+        #     node = self.tree[node_identifier]
+        #     parent = node.predecessor(self.tree.identifier)
+        #     successors = node.successors(self.tree.identifier)
+        #
+        #     node_list = [parent, node.identifier, node.data.type_, successors]
+        #
+        #     data.append(node_list)
+        #     print(parent, node.identifier, successors)
 
-        for node_identifier in self.tree.expand_tree(mode=TreeManager.WIDTH):
-            node = self.tree[node_identifier]
-            parent = node.predecessor(self.tree.identifier)
-            successors = node.successors(self.tree.identifier)
-
-            node_list = [parent, node.identifier, node.data.type_, successors]
-
-            data.append(node_list)
-            # print(parent, node.identifier, successors)
-
-        # data_df = pandas.DataFrame(data)
-        # data_df.to_csv(f"./{OUTPUT_PATH}/{context.setup.api_name}.i", header=False)
